da265d3b5b92e67a5ced45eb31c0d7969e34b488
John Teckemeyer <johnteckemeyer@gmail.com>
2021-08-02 23:02:30 -0700

    Create CustomButton component and use it to make a new task and filter buttons on homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
 import Header from '../../components/Header/Header';
+import CustomButton from '../../components/CustomButton/CustomButton';
+
+import './HomePage.scss';
 
 export default function HomePage (props) {
 
     return (
-        <>
+        <div>
             <Header menuOptions={props.menuOptions} logoutHandler={props.logoutHandler} />
-            <div>Home Page</div>
-        </>
+            <div className="homepage-container">
+                <div className="homepage-central-container">
+                    <CustomButton title="New Task" icon="add" />
+                    <CustomButton title="Filter" icon="sort" />
+                </div>
+            </div>
+        </div>
     )
 }
