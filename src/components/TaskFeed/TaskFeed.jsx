af70aad86bff603f86c9d999a7edec342fb41ea4
John Teckemeyer <johnteckemeyer@gmail.com>
2021-08-07 12:41:12 -0700

    Properly layout the task feed and make it scroll for many tasks

--- src/components/TaskFeed/TaskFeed.jsx
+++ src/components/TaskFeed/TaskFeed.jsx
@@ -4,7 +4,7 @@ import './TaskFeed.scss';
 export default function TaskFeed (props) {
     console.log(props.tasks)
     return (
-        <div style={{ border: '2px solid red' }}>
+        <div className="taskfeed-container">
             {
                 props.tasks.map(task => {
                     return <div key={task._id}>{task.description}</div>
