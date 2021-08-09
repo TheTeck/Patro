c7f405a14c8d9158cc09b12f648dc36ce2c1f4d3
John Teckemeyer <johnteckemeyer@gmail.com>
2021-08-09 16:37:16 -0700

    Style the task card and create a priority marker on it that shows the priority through color coding

--- src/components/TaskCard/TaskCard.jsx
+++ src/components/TaskCard/TaskCard.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TaskCard.scss';
 
 export default function TaskCard (props) {
-    return (
-        <div className="taskcard">
-            <div className="taskcard-header">
 
-            </div>
-            <div className="taskcard-content">
-                {props.task.description}
+    const [creator, setCreator] = useState("Anon");
+
+    const priorityClassString = `priority-marking priority-${props.task.priority}`;
+
+    return (
+        <div className="task-container">
+            <div className="taskcard">
+                <div className="taskcard-header">
+                    <div className={priorityClassString}> </div>
+                    <div className="task-creator">Created by {creator}</div>
+                </div>
+                <div className="taskcard-content">
+                    {props.task.description}
+                </div>
             </div>
         </div>
     );
