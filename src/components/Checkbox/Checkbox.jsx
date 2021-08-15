b1b9566cd09cccae4cdf366a18986941561d476e
John Teckemeyer <johnteckemeyer@gmail.com>
2021-08-14 22:48:40 -0700

    Create checkbox component and add it to the right of the task card

---
+++ src/components/Checkbox/Checkbox.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Checkbox.scss';
+
+export default function Checkbox (props) {
+    return (
+        <div className="checkbox">
+            <div className="material-icons md-48 check">
+                checkbox
+            </div>
+        </div>
+    );
+};
