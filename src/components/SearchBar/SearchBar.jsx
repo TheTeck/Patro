08c07d71b5170b20b8b703dabb48b042420bbef6
John Teckemeyer <johnteckemeyer@gmail.com>
2021-08-15 23:06:55 -0700

    Add search input to the searchbar component, in the style of Instagram's search

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -6,6 +6,7 @@ export default function SearchBar (props) {
     return (
         <div className="searchbar-container">
             <div className="material-icons md-36">search</div>
+            <input className="searchbar-input" placeholder="Search" />
         </div>
     );
 };
