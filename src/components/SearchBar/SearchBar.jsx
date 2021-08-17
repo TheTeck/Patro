094cd5366b78b117f03d9b36fe9eac86cfbdece8
John Teckemeyer <johnteckemeyer@gmail.com>
2021-08-16 23:02:41 -0700

    Add style button to the search and make the input controlled and lift the value of the input to the connection page if button is clicked

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './SearchBar.scss';
 
 export default function SearchBar (props) {
+
+    const [value, setValue] = useState('');
+
+    function handleUserFilter (e) {
+        props.showSeachResults(value);
+    };
+
+    function handleSearchInput (e) {
+        setValue(e.target.value);
+    };
+
     return (
         <div className="searchbar-container">
             <div className="material-icons md-36">search</div>
-            <input className="searchbar-input" placeholder="Search" />
+            <input onChange={handleSearchInput} className="searchbar-input" placeholder="Search" />
+            <button onClick={handleUserFilter} className="searchbar-btn">Go</button>
         </div>
     );
 };
