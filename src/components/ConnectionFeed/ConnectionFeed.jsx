0e97ad038f316079fa5d662d40a2b37177f20fff
John Teckemeyer <johnteckemeyer@gmail.com>
2021-08-28 21:43:14 -0700

    Create invitecard component and filter invited users through connectionsfeed component

--- src/components/ConnectionFeed/ConnectionFeed.jsx
+++ src/components/ConnectionFeed/ConnectionFeed.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 import ConnectionCard from '../ConnectionCard/ConnectionCard';
 import SearchResultCard from '../SearchResultCard/SearchResultCard';
+import InviteCard from '../InviteCard/InviteCard';
 import './ConnectionFeed.scss';
 
 export default function ConnectionFeed (props) {
+    console.log(props.filteredUsers)
     return (
         <div className="connection-feed-container">
             {
@@ -16,7 +18,7 @@ export default function ConnectionFeed (props) {
                                 {
                                     props.mode === "search" ? <SearchResultCard key={filteredUser._id} filteredUser={filteredUser} user={props.user} handleAddInvite={props.handleAddInvite} />
                                     : props.mode === "connections" ? <ConnectionCard key={filteredUser._id} connection={filteredUser} />
-                                    : <div>{filteredUser}</div>
+                                    : <InviteCard key={filteredUser._id} filteredUser={filteredUser} />
                                 }
                             </>
                         )
